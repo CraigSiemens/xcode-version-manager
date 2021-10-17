522a67a2ed23d3a177ec0774f564e6540de6f4c6
Craig Siemens <siemens.craig@gmail.com>
2021-10-16 23:19:08 -0600

    Removed unneeded test files

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-import XcodeVersionManagerTests
-import TableKitTests
-
-var tests = [XCTestCaseEntry]()
-tests += XcodeVersionManagerTests.allTests()
-tests += TableKitTests.allTests()
-XCTMain(tests)

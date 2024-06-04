b5ec3ddef42efe6caf5df85a66e77c1fbfa5451c
Craig Siemens <siemens.craig@gmail.com>
2024-06-04 16:10:39 -0600

    Moved the arguments for getting an installed Xcode version to it’s own ParsableArguments type.

---
+++ Sources/XcodeVersionManager/Commands/Arguments/InstalledXcodeVersion.swift
@@ -0,0 +1,79 @@
+import ArgumentParser
+
+struct InstalledXcodeVersion: ParsableArguments {
+    @Flag(
+        help: .init("Controls how the passed version is matched to an install of Xcode.")
+    )
+    var versionMatch: VersionMatch = .exact
+    
+    @Argument(
+        help: ArgumentHelp(
+            "The version number of Xcode to uninstall."
+        ),
+        completion: .custom { words in
+            do {
+                return try _unsafeWait {
+                    var formatter = XcodeVersionFormatter()
+                    formatter.separator = "-"
+                    
+                    return try await XcodeApplication
+                        .all()
+                        .map(formatter.string)
+                }
+            } catch {
+                return []
+            }
+        }
+    )
+    var version: String
+    
+    func xcodeApplication() async throws -> XcodeApplication {
+        var formatter = XcodeVersionFormatter()
+        formatter.separator = "-"
+        
+        let xcode = try await XcodeApplication
+            .all()
+            .sorted(by: versionMatch.order)
+            .first {
+                let formattedVersion = formatter.string(from: $0)
+                if versionMatch.isPrefix {
+                    return formattedVersion.hasPrefix(version)
+                } else {
+                    return formatter.string(from: $0) == version
+                }
+            }
+        
+        guard let xcode else {
+            throw CustomError("No version of Xcode found matching \"\(version)\"")
+        }
+        
+        return xcode
+    }
+}
+
+// MARK: - VersionMatch
+extension InstalledXcodeVersion {
+    enum VersionMatch: EnumerableFlag {
+        case exact
+        case oldestWithPrefix
+        case newestWithPrefix
+        
+        var isPrefix: Bool {
+            switch self {
+            case .exact:
+                false
+            case .oldestWithPrefix, .newestWithPrefix:
+                true
+            }
+        }
+        
+        func order(lhs: XcodeApplication, rhs: XcodeApplication) throws -> Bool {
+            switch self {
+            case .exact, .oldestWithPrefix:
+                lhs < rhs
+            case .newestWithPrefix:
+                lhs > rhs
+            }
+        }
+    }
+}
--- Sources/XcodeVersionManager/Commands/UninstallCommand.swift
+++ Sources/XcodeVersionManager/Commands/UninstallCommand.swift
@@ -13,51 +13,10 @@ struct UninstallCommand: AsyncParsableCommand {
     )
     var force: Bool = false
     
-    @Flag(
-        help: .init("Controls how the passed version is matches to an install of Xcode.")
-    )
-    var versionMatch: VersionMatch = .exact
-    
-    @Argument(
-        help: ArgumentHelp(
-            "The version number of Xcode to uninstall."
-        ),
-        completion: .custom { words in
-            do {
-                return try _unsafeWait {
-                    var formatter = XcodeVersionFormatter()
-                    formatter.separator = "-"
-                    
-                    return try await XcodeApplication
-                        .all()
-                        .map(formatter.string)
-                }
-            } catch {
-                return []
-            }
-        }
-    )
-    var version: String
+    @OptionGroup var xcodeVersion: InstalledXcodeVersion
     
     func run() async throws {
-        var formatter = XcodeVersionFormatter()
-        formatter.separator = "-"
-        
-        let xcode = try await XcodeApplication
-            .all()
-            .sorted(by: versionMatch.order)
-            .first {
-                let formattedVersion = formatter.string(from: $0)
-                if versionMatch.isPrefix {
-                    return formattedVersion.hasPrefix(version)
-                } else {
-                    return formatter.string(from: $0) == version
-                }
-            }
-        
-        guard let xcode else {
-            throw CustomError("No version of Xcode found matching \"\(version)\"")
-        }
+        let xcode = try await xcodeVersion.xcodeApplication()
         
         guard force || askConfirmation(
             "Are you sure you want to uninstall \(xcode.url.lastPathComponent)?"
@@ -66,31 +25,5 @@ struct UninstallCommand: AsyncParsableCommand {
         }
         
         try FileManager.default.removeItem(at: xcode.url)
-    }
-}
-
-extension UninstallCommand {
-    enum VersionMatch: EnumerableFlag {
-        case exact
-        case oldestWithPrefix
-        case newestWithPrefix
-        
-        var isPrefix: Bool {
-            switch self {
-            case .exact: 
-                false
-            case .oldestWithPrefix, .newestWithPrefix:
-                true
-            }
-        }
-        
-        func order(lhs: XcodeApplication, rhs: XcodeApplication) throws -> Bool {
-            switch self {
-            case .exact, .oldestWithPrefix:
-                lhs < rhs
-            case .newestWithPrefix:
-                lhs > rhs
-            }
-        }
     }
 }
